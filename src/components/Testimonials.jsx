cd3bdc7f3e402ef009c273e984fe6308915ab95f
apoorv <mrapoorvchaturvedi7@gmail.com>
2026-06-10 05:50:38 +0530

    done now

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,24 +11,24 @@ import avatar7 from "../assets/testimonials/avatar7.jpg";
 import avatar8 from "../assets/testimonials/avatar8.png";
 
 const avatars = [
-  { src: avatar1, className: "left-[8%] top-[2%] h-20 w-20" },
-  { src: avatar2, className: "left-[2%] top-[32%] h-12 w-12" },
-  { src: avatar3, className: "left-[10%] top-[52%] h-28 w-28" },
-  { src: avatar4, className: "left-[2%] top-[72%] h-20 w-20" },
-  { src: avatar5, className: "right-[10%] top-[2%] h-20 w-20" },
-  { src: avatar6, className: "right-[25%] top-[14%] h-16 w-16" },
-  { src: avatar7, className: "right-[18%] top-[38%] h-12 w-12" },
-  { src: avatar8, className: "right-[2%] top-[58%] h-36 w-36 lg:h-44 lg:w-44" },
+  { src: avatar1, className: "left-[10%] top-[12%] h-28 w-28" },
+  { src: avatar2, className: "left-[3%] top-[42%] h-14 w-14" },
+  { src: avatar3, className: "left-[11%] top-[45%] h-44 w-44" },
+  { src: avatar4, className: "left-[2%] top-[74%] h-24 w-24" },
+  { src: avatar5, className: "right-[6%] top-[12%] h-32 w-32" },
+  { src: avatar6, className: "right-[22%] top-[24%] h-20 w-20" },
+  { src: avatar7, className: "right-[18%] top-[42%] h-24 w-24" },
+  { src: avatar8, className: "right-[4%] top-[58%] h-56 w-56 lg:h-64 lg:w-64" },
 ];
 
 export default function Testimonials() {
   return (
-    <SectionContainer className="py-16 sm:py-20 lg:py-24" innerClassName="relative min-h-[420px]">
+    <SectionContainer className="py-24 sm:py-32 lg:py-40" innerClassName="relative min-h-[600px]">
       <div className="text-center">
         <div className="relative inline-block">
-          <div className="absolute left-0 top-2 -z-10 h-10 w-24 rounded-full bg-[#DDEBDB] sm:h-12 sm:w-28" />
+          <div className="absolute left-0 top-0 -z-10 h-12 w-28 rounded-full bg-[#c3d3c0] sm:h-12 sm:w-36" />
 
-          <h2 className="text-4xl font-light leading-tight text-black sm:text-5xl">
+          <h2 className="text-4xl font-light gerbil-font leading-tight text-black sm:text-5xl">
             What our customer
             <br />
             says About Us
@@ -36,7 +36,7 @@ export default function Testimonials() {
 
           <Yellow
             strokeWidth={7}
-            className="absolute left-[18%] top-[78%] -z-10 h-5 w-[64%]"
+            className="absolute left-[38%] top-[90%] -z-10 h-8 w-[64%]"
           />
         </div>
       </div>
@@ -62,12 +62,12 @@ export default function Testimonials() {
       ))}
 
       <div className="relative z-10 mt-10 flex justify-center md:mt-16">
-        <div className="relative w-full max-w-2xl rounded-[32px] bg-[#EEF2EE] px-6 py-9 sm:px-10 sm:py-10">
+        <div className="relative w-full max-w-2xl rounded-[32px] bg-[#D7EEDD4D] px-6 py-9 sm:px-10 sm:py-10">
           <span className="absolute left-4 top-2 text-6xl leading-none text-[#CFCFCF] sm:left-6 sm:top-4 sm:text-7xl">
             &ldquo;
           </span>
 
-          <p className="px-2 text-center text-base leading-8 text-[#333] sm:px-6">
+          <p className="px-2 text-center text-base gerbil-font leading-7 text-[#535453] sm:px-6">
             Elementum delivered the site within the timeline as they requested.
             In the end, the client found a 50% increase in traffic within days
             since its launch. They also had an impressive ability to use
